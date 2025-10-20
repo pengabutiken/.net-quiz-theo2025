c1f727a57f2019530add1d85b35ba40b001a641a
Theo Björkqvist <113356799+pengabutiken@users.noreply.github.com>
2025-10-20 12:38:07 +0200

    gjort klart modellen

--- obj/Models/Questions.cs
+++ obj/Models/Questions.cs
@@ -1,6 +1,12 @@
 using System.Collections.Generic;
+using System.Dynamic;
 
 namespace QuizTheo2025.Models
 {
-
+    public class Questions
+    {
+        public string imageUrl { get; set; }
+        public List<string> Options { get; set; }
+        public int correctIndex { get; set; }
+    }
 }
